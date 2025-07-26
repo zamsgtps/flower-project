baf1778f914f7f3541ea2d3688fc8caa373024f3
zamsgtps <zamsgg6@gmail.com>
2025-07-26 20:13:29 +0700

    script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ onload = () => {
     document.body.classList.remove("not-loaded");
     clearTimeout(c);
   }, 1000);
+  window.addEventListener('click', () => {
+  const audio = document.getElementById('bg-music');
+  if (audio && audio.paused) {
+    audio.play();
+  }
+});
 };
